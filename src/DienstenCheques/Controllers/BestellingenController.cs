ba971f21010ba121c152e7785703fab3487a1cbf
ksa607 <karine.samyn@hogent.be>
2016-12-14 14:19:04 +0100

    Implement assignment 8

--- src/DienstenCheques/Controllers/BestellingenController.cs
+++ src/DienstenCheques/Controllers/BestellingenController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DienstenCheques.Controllers
 {
     [ServiceFilter(typeof(GebruikerFilter))]
+    [Authorize(Policy = "customer")]
     public class BestellingenController : Controller
     {
         private readonly IGebruikersRepository _gebruikersRepository;
--- src/DienstenCheques/Filters/GebruikerFilter.cs
+++ src/DienstenCheques/Filters/GebruikerFilter.cs
@@ -6,17 +6,18 @@ namespace DienstenCheques.Filters
 {
     public class GebruikerFilter : ActionFilterAttribute
     {
-       private readonly IGebruikersRepository _gebruikerRepository;
-            private Gebruiker _gebruiker;
+        private readonly IGebruikersRepository _gebruikerRepository;
+        private Gebruiker _gebruiker;
 
-            public GebruikerFilter(IGebruikersRepository gebruikerRepository)
-            {
+        public GebruikerFilter(IGebruikersRepository gebruikerRepository)
+        {
             _gebruikerRepository = gebruikerRepository;
-            }
+        }
 
-            public override void OnActionExecuting(ActionExecutingContext context)
-            {
-             _gebruiker = _gebruikerRepository.GetBy(1); 
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+                _gebruiker = _gebruikerRepository.GetByEmail(context.HttpContext.User.Identity.Name);
             context.ActionArguments["gebruiker"] = _gebruiker;
             base.OnActionExecuting(context);
         }
